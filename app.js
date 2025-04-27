fa830dd437ad9929dbbea29c2fc9abe3ff578ee5
114514ns <121270969+114514ns@users.noreply.github.com>
2025-04-27 09:33:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, world!',
-  })
-})
+app.use('/', createProxyMiddleware({
+  target: 'https://api.bilibili.com', // 目标服务器
+  changeOrigin: true,             // 修改请求头中的 Origin
+  pathRewrite: { '^/': '/' },     // 可选：重写路径
+}));
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
