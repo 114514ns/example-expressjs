47a0f1302cb42e0d9c0d7a03238f29ca12702dc8
114514ns <121270969+114514ns@users.noreply.github.com>
2025-04-29 10:27:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use((req, res, next) => {
   req.startTime = Date.now();
   next();
 });
-
+/*
 app.use('/', createProxyMiddleware({
   target: 'https://api.bilibili.com', // 目标服务器
   changeOrigin: true,             // 修改请求头中的 Origin
@@ -16,6 +16,7 @@ app.use('/', createProxyMiddleware({
     res.setHeader('X-Response-Time', `${duration}ms`);
   }
 }));
+*/
 app.get('/ip', async (req, res) => {
   const response = await fetch('https://api.ipify.org');
   const ip = await response.text();
